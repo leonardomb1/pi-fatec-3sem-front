9e0fb693956979676747e1a1cb26628e8b57f200
Luiz Felipe <luiz.felipe@smartinnovation.com.br>
2024-11-18 11:07:58 -0300

    FEAT: created base service

---
+++ apps/senai/src/services/base-service.service.ts
@@ -0,0 +1,115 @@
+import { HttpClient, HttpHeaders, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { environment } from '../enviroments/enviroment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BaseServiceService {
+ private apiUrl = environment.apiUrl;
+
+ constructor(private http: HttpClient) { }
+
+ private getHeaders() {
+   return new HttpHeaders({
+     'Content-Type': 'application/json',
+     'Authorization': `Bearer ${this.getToken()}`
+   });
+ }
+
+ makeGet<T>(endpoint: string, params?: { [key: string]: string }, queryParams?: { [key: string]: string }): Observable<T> {
+  let httpParams = new HttpParams();
+
+  if (params) {
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        httpParams = httpParams.set(key, params[key]);
+      }
+    }
+  }
+
+  if (queryParams) {
+    for (const key in queryParams) {
+      if (queryParams.hasOwnProperty(key)) {
+        httpParams = httpParams.set(key, queryParams[key]);
+      }
+    }
+  }
+
+  return this.http.get<T>(this.apiUrl + endpoint, { headers: this.getHeaders(), params: httpParams })
+    .pipe(catchError(this.handleError));
+}
+
+makePost<T>(endpoint: string, body: any, queryParams?: { [key: string]: string }): Observable<T> {
+  let httpParams = new HttpParams();
+
+  if (queryParams) {
+    for (const key in queryParams) {
+      if (queryParams.hasOwnProperty(key)) {
+        httpParams = httpParams.set(key, queryParams[key]);
+      }
+    }
+  }
+
+  return this.http.post<T>(this.apiUrl + endpoint, body, { headers: this.getHeaders(), params: httpParams })
+    .pipe(catchError(this.handleError));
+}
+
+makePut<T>(endpoint: string, body: any, queryParams?: { [key: string]: string }): Observable<T> {
+  let httpParams = new HttpParams();
+
+  if (queryParams) {
+    for (const key in queryParams) {
+      if (queryParams.hasOwnProperty(key)) {
+        httpParams = httpParams.set(key, queryParams[key]);
+      }
+    }
+  }
+
+  return this.http.put<T>(this.apiUrl + endpoint, body, { headers: this.getHeaders(), params: httpParams })
+    .pipe(catchError(this.handleError));
+}
+
+makeDelete<T>(endpoint: string, params?: { [key: string]: string }, queryParams?: { [key: string]: string }): Observable<T> {
+  let httpParams = new HttpParams();
+
+  if (params) {
+    for (const key in params) {
+      if (params.hasOwnProperty(key)) {
+        httpParams = httpParams.set(key, params[key]);
+      }
+    }
+  }
+
+  if (queryParams) {
+    for (const key in queryParams) {
+      if (queryParams.hasOwnProperty(key)) {
+        httpParams = httpParams.set(key, queryParams[key]);
+      }
+    }
+  }
+
+  return this.http.delete<T>(this.apiUrl + endpoint, { headers: this.getHeaders(), params: httpParams })
+    .pipe(catchError(this.handleError));
+}
+
+ private getToken(): string {
+   return localStorage.getItem('auth_token') || '';
+ }
+
+ private handleError(error: HttpErrorResponse) {
+   console.error('Erro na requisição:', error);
+
+   switch(error.status) {
+     case 400:
+       return throwError(() => new Error('Requisição inválida'));
+     case 404:
+       return throwError(() => new Error('Recurso nao encontrado'));
+     case 500:
+       return throwError(() => new Error('Erro no servidor'));
+     default:
+       return throwError(() => new Error('Erro' || error.message));
+   }
+ }
+}
