c36c56ce7f909ae74c8985e0e8aa31236d91865e
Luiz Felipe <luiz.felipe@smartinnovation.com.br>
2024-11-27 18:54:50 -0300

    FEAT: added images

--- apps/senai/src/app/images.ts
+++ apps/senai/src/app/images.ts
@@ -1,4 +1,8 @@
 export const randomImages: string[] = [
-  'instagram.png',
-  'linkedin.png',
+  'imagem1.jpg',
+  'imagem2.jpg',
+  'imagem3.jpg',
+  'imagem4.jpeg',
+  'imagem5.jpeg',
+  'imagem6.jpeg',
 ]
